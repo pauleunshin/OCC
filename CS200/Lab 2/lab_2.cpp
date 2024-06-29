9d64974eba1ffdcc78d86e67d5c6356df98c3fb6
Paul <paul.eun.shin@gmail.com>
2024-06-28 20:55:19 -0700

    Lab 2 fix and lab 3 setup

--- CS200/Lab 2/lab_2.cpp
+++ CS200/Lab 2/lab_2.cpp
@@ -3,24 +3,30 @@
 list<int>& bfs(const vector<vector<int>>& graph, list<int>& result, const int start) {
     const int SIZE = graph.size();
     bool* arr = new bool[SIZE];
+    int limit = 0;
+
     arr[start] = true;
     queue<int> order;
     order.push(start);
 
     while (!order.empty()) {
         int counter = 0;
-        for (auto iter : graph[order.front()]) {
-            if (iter == 1) {
-                if (arr[counter] != true) {
-                    arr[counter] = true;
-                    order.push(counter);
+        if (limit != SIZE) {
+            for (auto iter : graph[order.front()]) {
+                if (iter == 1) {
+                    if (arr[counter] != true) {
+                        arr[counter] = true;
+                        order.push(counter);
+                        limit++;
+                    }
                 }
+                counter++;
             }
-            counter++;
         }
         result.emplace_back(order.front());
         order.pop();
     }
+
     delete[] arr;
     arr = nullptr;
     return result;
